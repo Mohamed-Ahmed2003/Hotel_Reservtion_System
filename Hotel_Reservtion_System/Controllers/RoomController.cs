5b624fdbf0116b273a7111beb6d6007b205c617c
mohamed <moham@DESKTOP-R03HSOC>
2025-09-29 09:05:42 +0300

    make room controller to add , get and delete rooms

---
+++ Hotel_Reservtion_System/Controllers/RoomController.cs
@@ -0,0 +1,95 @@
+﻿using Hotel_Reservtion_System.DatabaseContext;
+using Hotel_Reservtion_System.DTO;
+using Hotel_Reservtion_System.Entity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hotel_Reservtion_System.Controllers
+{
+    [ApiController]
+    public class RoomController : ControllerBase
+    {
+        private readonly HoteldbContext _context;
+        public RoomController(HoteldbContext context)
+        {
+            _context = context;
+        }
+
+        [Route("api/creatRoom")]
+        [HttpPost]
+        [Authorize(Roles = "admin")]
+        public async Task<IActionResult> creatRoom(RoomDTO room)
+        {
+            if (ModelState.IsValid)
+            {
+                Room newRoom = room.convertToRoom();
+                newRoom.branch = await _context.branches.FirstOrDefaultAsync(b => b.id == room.branchID);
+                if (newRoom.branch == null)
+                {
+                    return BadRequest("branch not found");
+                }
+                await _context.rooms.AddAsync(newRoom);
+                await _context.SaveChangesAsync();
+                return Ok(newRoom);
+            }
+            else
+            {
+                String errors = string.Join("; ", ModelState.Values
+                                       .SelectMany(x => x.Errors)
+                                       .Select(x => x.ErrorMessage));
+                return BadRequest(errors);
+            }
+        }
+
+        [Route("api/getAllRooms/{branchID}")]
+        [HttpGet]
+        [Authorize(Roles = "admin,employee")]
+        public async Task<IActionResult> getAllRooms(Guid branchID)
+        {
+            IEnumerable<Room> rooms = await _context.rooms.Where(r => r.branch != null && r.branch.id == branchID).Include(r =>  r.branch)
+                .ToListAsync();
+            return Ok(rooms);
+        }
+
+        [Route("api/getAvailableRooms/{branchID}")]
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> getAvailableRooms(Guid branchID)
+        {
+            IEnumerable<Room> rooms = await _context.rooms.Where(r => r.branch != null && r.branch.id == branchID && r.availability == true).Include(r => r.branch)
+                .ToListAsync();
+           
+            return Ok(rooms);
+        }
+
+        [Route("api/updateRoom/{roomID}")]
+        [HttpPut]
+        [Authorize(Roles = "admin,employee")]
+        public async Task<IActionResult> updateRoom(Guid roomID, RoomDTO room)
+        {
+            if (ModelState.IsValid)
+            {
+                Room? existingRoom = await _context.rooms.Include(r=>r.branch).FirstOrDefaultAsync(r => r.id == roomID);
+                if (existingRoom == null)
+                {
+                    return NotFound("Room not found");
+                }
+                existingRoom.roomType = room.roomType;
+                existingRoom.roomCode = room.roomCode;
+                existingRoom.price = room.price;
+                existingRoom.availability = room.availability;
+                await _context.SaveChangesAsync();
+                return Ok(existingRoom);
+            }
+            else
+            {
+                String errors = string.Join("; ", ModelState.Values
+                                       .SelectMany(x => x.Errors)
+                                       .Select(x => x.ErrorMessage));
+                return BadRequest(errors);
+            }
+        }
+    }
+}
---
+++ Hotel_Reservtion_System/DTO/RoomDTO.cs
@@ -0,0 +1,48 @@
+﻿using Hotel_Reservtion_System.Entity;
+using Microsoft.AspNetCore.DataProtection.KeyManagement.Internal;
+using System.ComponentModel.DataAnnotations;
+
+namespace Hotel_Reservtion_System.DTO
+{
+    public class RoomDTO
+    {
+        [Required(ErrorMessage = "Room type is required")]
+        public string? roomType { get; set; }
+        [Required(ErrorMessage = "Room code is required")]
+        public string? roomCode { get; set; }
+        [Required(ErrorMessage = "Room price is required")]
+        [Range(0, double.MaxValue, ErrorMessage = "Price must be a positive number")]
+        public double? price { get; set; }
+
+        public bool availability { get; set; } = true;
+        [Required(ErrorMessage = "branch is required")]
+        public Guid? branchID { get; set; }
+
+
+
+        public Room convertToRoom()
+        {
+            Room room = new Room();
+            room.id = Guid.NewGuid();
+            room.roomType = this.roomType;
+            room.roomCode = this.roomCode;
+            room.price = this.price;
+            room.availability = this.availability;
+            return room;
+        }
+       /* public RoomDTO convertToDto(Room room)
+        {
+            RoomDTO roomDTO = new RoomDTO();
+            roomDTO.roomType = room.roomType;
+            roomDTO.roomCode = room.roomCode;
+            roomDTO.price = room.price;
+            roomDTO.availability = room.availability;
+            if (room.branch != null)
+            {
+                roomDTO.branchID = room.branch.id;
+            }
+            return roomDTO;
+        }
+*/
+    }
+}
